532a64c3c1494b54076a10feda4055d7b54a2f2a
crexpoxjr <hecdiermaldonado@gmail.com>
2026-03-24 11:05:05 -0500

    new file:   DriverDatabase.cpp

---
+++ DriverDatabase.cpp
@@ -0,0 +1,38 @@
+#include "Driver.cpp"
+#include "HashTable.cpp"
+#include "LinkedList.cpp"
+
+class DriverDatabase{
+    private:
+    HashTable activeDrivers;
+    HashTable inactiveDrivers;
+    LinkedList licenseList;
+
+    public:
+    void addDriver(){
+
+    }
+
+    void removeDriver(){
+
+    }
+
+    Driver findDriver(int searchID){
+
+    }
+
+    void moveToInactive(int id){
+
+    }
+
+    void getOldestLicenses(int n){
+
+    }
+
+    void getNewestLicenses(int n){
+        
+    }
+
+
+
+};
